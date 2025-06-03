26e6a7d47e8d25ed3607c4638482d5c26ed6b069
Claudio Vargas Saavedra <claudsaavedrav@gmail.com>
2025-06-02 21:04:09 -0400

    feat: soft delete and index available

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma';
 import { PaginationDto } from 'src/common';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -22,13 +27,14 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   async findAll(paginationDto: PaginationDto) {
     const { page, limit } = paginationDto;
 
-    const totalPages = await this.product.count();
+    const totalPages = await this.product.count({ where: { available: true } });
     const lastPage = Math.ceil(totalPages / limit);
 
     return {
       data: await this.product.findMany({
         skip: (page - 1) * limit,
         take: limit,
+        where: { available: true },
       }),
       meta: {
         total: totalPages,
@@ -39,9 +45,12 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   }
 
   async findOne(id: number) {
-    const product =  await this.product.findUnique({
-      where: { id },
-    })
+    const product = await this.product.findUnique({
+      where: {
+        id,
+        available: true,
+      },
+    });
 
     if (!product) {
       throw new NotFoundException(`Product with ID ${id} not found`);
@@ -53,12 +62,23 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     await this.findOne(id); // Ensure the product exists before updating
 
     return this.product.update({
-      where: {id: id},
+      where: { id: id },
       data: updateProductDto,
-    })
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    await this.findOne(id); // Ensure the product exists before updating
+
+    // return this.product.delete({
+    //   where: { id },
+    // });
+    const product = await this.product.update({
+      where: { id },
+      data: {
+        available: false,
+      },
+    });
+    return product;
   }
 }
